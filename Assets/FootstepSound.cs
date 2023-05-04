99acc69de0d4c0959ea949e7ad10a326ffd7f25b
Roger <79161186+Draquian@users.noreply.github.com>
2023-05-04 20:30:31 +0200

    escena + alguns audios

--- Assets/FootstepSound.cs
+++ Assets/FootstepSound.cs
@@ -6,6 +6,9 @@ public class FootstepSound : MonoBehaviour
 {
     public AudioClip[] footstepsOnGrass;
     public AudioClip[] footstepsOnWood;
+    public AudioClip[] footstepsOnStone;
+    public AudioClip[] footstepsOnDirt;
+    public AudioClip[] footstepsOnFlower;
 
     public string material;
 
@@ -27,6 +30,20 @@ public class FootstepSound : MonoBehaviour
                     audioSource.PlayOneShot(footstepsOnWood[Random.Range(0, footstepsOnWood.Length)]);
                 break;
 
+            case "Stone":
+                if (footstepsOnStone.Length > 0)
+                    audioSource.PlayOneShot(footstepsOnStone[Random.Range(0, footstepsOnStone.Length)]);
+                break;
+
+            case "Dirt":
+                if (footstepsOnDirt.Length > 0)
+                    audioSource.PlayOneShot(footstepsOnDirt[Random.Range(0, footstepsOnDirt.Length)]);
+                break;
+
+            case "Flower":
+                if (footstepsOnFlower.Length > 0)
+                    audioSource.PlayOneShot(footstepsOnFlower[Random.Range(0, footstepsOnFlower.Length)]);
+                break;
             default:
                 break;
         }
@@ -38,6 +55,9 @@ public class FootstepSound : MonoBehaviour
         {
             case "Grass":
             case "Wood":
+            case "Stone":
+            case "Dirt":
+            case "Flower":
                 material = collision.gameObject.tag;
                 break;
 
